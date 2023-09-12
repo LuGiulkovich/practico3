ef209226cd2c064a4e284b2a7185d5e4571362cf
LuGiulkovich <lugiulkovich@gmail.com>
2023-09-12 19:47:20 -0300

    Modifico Parte 4

--- src/componentes/EleccionJugadas.js
+++ src/componentes/EleccionJugadas.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import { JugadaPC } from './JugadaPC';
 
 /** Componente que muestra la opcion elegida, tanto del boton del usuario como la de la pc */
 export const EleccionJugadas = ({siCambiarEleccionJugador, siJugadaAleatoria}) => {
   /** Este estado devuelve el texto del boton. String */
   const [eleccionJugador, setEleccionJugador] = useState('');
+  const [eleccionPC, setEleccionPC] = useState(0);
 
   /** Creo una funcion que me actualice el valor del boton seleccionado */
   const handleClick = (textoBoton) => {
@@ -12,13 +12,19 @@ export const EleccionJugadas = ({siCambiarEleccionJugador, siJugadaAleatoria}) =
     siCambiarEleccionJugador(textoBoton);
   }
 
+  /** Funcion para la jugada aleatoria */
+  const jugadaAleatoria = () => {
+    setEleccionPC(Math.floor(Math.random() * (2 - 0 + 1) + 0));
+    siJugadaAleatoria(eleccionPC);
+}
+
   return (
     <div className='EleccionJugadas'>
       <h2>Opciones de Juego</h2>
       <button onClick={() => handleClick('Piedra')}>Piedra</button>
       <button onClick={() => handleClick('Papel')}>Papel</button>
       <button onClick={() => handleClick('Tijeras')}>Tijeras</button>
-      <JugadaPC siJugadaAleatoria={siJugadaAleatoria}/>
+      <button onClick={jugadaAleatoria}>Jugar</button>
     </div>
   )
 }
